4e5af6947ffcce4c8ff0a0b816a63d0a923a9574
cbl09 <binglingcai1228@gmail.com>
2017-03-20 23:38:12 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -9,28 +9,30 @@ import imageio
 class Lung_nodule:
     def __init__(self, master):
         self.master = master
-        master.title(Lung_nodule)
+        master.title('Lung_nodule')
         master.geometry("1800x1600")
         frame0 = Frame(master) #创建一个框架
         frame0.pack()
 
-        bt0 = Button(frame0, text='File', highlightbackground='#3E4149', command=self.loadData)
-        bt1 = Button(frame0, text='P1', command=self.Segmentation)
-        bt2 = Button(frame0, text='P2', command=self.Detection)
-        bt3 = Button(frame0, text='CLEAR ALL', command=self.ClearAll)
+        bt0 = Button(frame0, text='Load File', font=("黑体", 15), height=2, width=10, command=self.loadData)
+        bt1 = Button(frame0, text='Segment', font=("黑体", 15), command=self.Segmentation, height=2, width=10)
+        bt2 = Button(frame0, text='Detect', font=("黑体", 15), command=self.Detection, height=2, width=10)
 
-        m1 = Label(frame0, text='Please select the image file.')
-        m2 = Label(frame0, text='Do the Segmentation!')
-        m3 = Label(frame0, text='Do the detection!')
+        m1 = Label(frame0, text='Please select the image file.', font=("黑体", 15))
+        m2 = Label(frame0, text='Do the Segmentation!', font=("黑体", 15))
+        m3 = Label(frame0, text='Do the detection!', font=("黑体", 15))
 
-        bt0.grid(row=1, ipadx=20, ipady=5)
-        bt1.grid(row=2, ipadx=20)
-        bt2.grid(row=3, ipadx=20)
-        bt3.grid(row=1, column=30, ipadx=20, ipady=5)
+        bt3 = Button(frame0, text='CLEAR ALL', font=("黑体", 15), height=2, width=10, command=self.ClearAll)
 
-        m1.grid(row=1, column=5, sticky=W)
-        m2.grid(row=2, column=5, sticky=W)
-        m3.grid(row=3, column=5, sticky=W)
+        bt0.grid(row=0)
+        bt1.grid(row=1)
+        bt2.grid(row=2)
+
+        m1.grid(row=0, column=5, sticky=W)
+        m2.grid(row=1, column=5, sticky=W)
+        m3.grid(row=2, column=5, sticky=W)
+
+        bt3.grid(row=0, column=65, sticky=W)
 
     def loadData(self):
         ftypes = [('Image Files', '*.tif *.jpg *.png')]
@@ -41,26 +43,27 @@ class Lung_nodule:
         photo = PIL.ImageTk.PhotoImage(img)
         self.label0 = Label(image=photo)
         self.label0.image = photo #keep a reference
-        self.label0.pack(side=LEFT)
+        self.label0.place(x=0, y=300)
       
     def Segmentation(self):
         video_name = '1.mp4'
         self.video = imageio.get_reader(video_name)
         self.vedio_show = Label()
-        self.vedio_show.pack(side=LEFT)
+        self.vedio_show.place(x=400,y=150)
         thread = threading.Thread(target=self.stream, args=(self.vedio_show,))
         thread.setDaemon(True)
         thread.start()
         print('segment!')
 
     def Detection(self):
-        img = PIL.Image.open('309_1.tif')
-        #img.thumbnail((450,450),Image.ANTIALIAS)
-        photo = PIL.ImageTk.PhotoImage(img)
-        self.label2 = Label(image=photo)
-        self.label2.image = photo #keep a reference
-        self.label2.pack(side=LEFT)
-        print('detect!')
+        video_name = '1.mp4'
+        self.video = imageio.get_reader(video_name)
+        self.label2 = Label()
+        self.label2.place(x=900,y=150)
+        thread = threading.Thread(target=self.stream, args=(self.label2,))
+        thread.setDaemon(True)
+        thread.start()
+        print('detection!')
 
     def stream(self,label):
         for image in self.video.iter_data():
